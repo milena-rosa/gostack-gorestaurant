910b903c8dfbceed2a5904a20fbd0e4eb1f49d5e
Milena Boselli Rosa <milena.rosa@gmail.com>
2020-07-12 19:43:48 -0300

    adicionadas funcionalidades de criaçao, ediçao, listagem e remoçao de pratos.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('foods');
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +38,11 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const newId = foods.length + 1;
+      const newFood = { id: newId, ...food, available: true };
+
+      const response = await api.post('foods', newFood);
+      setFoods([...foods, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +51,19 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const { id } = editingFood;
+    const updatedFood = { id, ...food, available: true };
+
+    const response = await api.put(`foods/${id}`, updatedFood);
+
+    const updatedState = [foods.filter(item => item.id !== id), response.data];
+    setFoods(updatedState);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`foods/${id}`);
+
+    setFoods(foods.filter(food => food.id !== id));
   }
 
   function toggleModal(): void {
@@ -62,7 +75,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    setEditModalOpen(true);
   }
 
   return (
@@ -84,7 +98,7 @@ const Dashboard: React.FC = () => {
         {foods &&
           foods.map(food => (
             <Food
-              key={food.id}
+              key={String(food.id)}
               food={food}
               handleDelete={handleDeleteFood}
               handleEditFood={handleEditFood}
